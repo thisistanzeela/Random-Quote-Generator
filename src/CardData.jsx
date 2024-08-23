22aab168124417d45b9fa3a13e056644269e9e7d
thisistanzeela <thisistanzeela98@gmail.com>
2024-08-23 23:31:10 +0530

    Cleanup

--- src/CardData.jsx
+++ src/CardData.jsx
@@ -3,14 +3,11 @@ let CardData = ({ quote, author, onChangeQuote }) => {
     return (
       <div className="max-w-sm mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-6">
         <div className="md:flex">
-          <div className="md:flex-shrink-0">
-            {/* You can add an image or icon here if needed */}
-          </div>
           <div className="mt-4 md:mt-0 md:ml-6">
             <h1 className="text-xl font-semibold text-gray-900">{quote}</h1>        
             <h4 className="text-gray-600 mt-2">{author}</h4>  
             <button 
-              className="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 my-4 rounded"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-4 rounded"
               onClick={onChangeQuote}
             >
               Change Quote
