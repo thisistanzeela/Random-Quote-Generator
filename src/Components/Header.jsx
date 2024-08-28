1918cfe7a7d748c5c7b8cf6c5cf4f3ad072284d8
thisistanzeela <thisistanzeela98@gmail.com>
2024-08-28 22:24:07 +0530

    Router

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,30 +1,36 @@
 import { useContext } from "react";
 import UserContext from "../utils/UserContext";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    const {user} = useContext(UserContext);
-    // console.log(user)
-    return (
-      <div className="flex items-center justify-between p-4 bg-gray-800 text-white">
-        <div className="flex items-center">
-          <img
-            src="https://e7.pngegg.com/pngimages/639/449/png-clipart-computer-icons-website-icon-text-globe.png"
-            alt="Logo"
-            className="h-8 w-8 mr-2"
-          />
-          <span className="text-xl font-bold">My Website</span>
-        </div>
-        <div>
-          <ul className="flex space-x-4">
-            <li className="hover:text-gray-300 cursor-pointer">Home</li>
-            <li className="hover:text-gray-300 cursor-pointer">About</li>
-            <li className="hover:text-gray-300 cursor-pointer">Contact</li>
-            <li className="hover:text-gray-300 cursor-pointer">{user}</li>
-          </ul>
-        </div>
+  const { user } = useContext(UserContext);
+
+  return (
+    <div className="flex items-center justify-between p-4 bg-gray-800 text-white">
+      <div className="flex items-center">
+        <img
+          src="https://e7.pngegg.com/pngimages/639/449/png-clipart-computer-icons-website-icon-text-globe.png"
+          alt="Logo"
+          className="h-8 w-8 mr-2"
+        />
+        <span className="text-xl font-bold">My Website</span>
       </div>
-    );
-  };
-  
-  export default Header;
-  
+      <div>
+        <ul className="flex space-x-4">
+          <li className="hover:text-gray-300 cursor-pointer">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="hover:text-gray-300 cursor-pointer">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="hover:text-gray-300 cursor-pointer">
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li className="hover:text-gray-300 cursor-pointer">{user}</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Body from './Components/Body'
-import Header from './Components/Header'
-import './index.css';
+import { StrictMode } from "react";
+import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+import Body from "./Components/Body";
+import Header from "./Components/Header";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import "./index.css";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import ErrorComponent from "./Components/ErrorComponent";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Header />
-    <Body />
-  </StrictMode>,
-)
+const App = () =>{
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  )
+}
+
+const AppRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path : "/",
+        element : <Body />
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      // {
+      //   path: "/contact",
+      //   element: <Contact />,
+      // },
+    ],
+    errorElement: <ErrorComponent />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RouterProvider router={AppRouter} />)
